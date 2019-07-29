2547e6765bdbf74da87e897248112cf52b147b23
Vaibhav Desai <vaibhavdesai16@gmail.com>
2019-07-29 16:58:47 +0530

    feature scaling

--- csv_processing.py
+++ csv_processing.py
@@ -21,3 +21,18 @@ labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 onehotencoder = OneHotEncoder(categorical_features=[0])
 X = onehotencoder.fit_transform(X).toarray()
+
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
+#splitting data into the trainning a set and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_Test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+#Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_train)
+
+
